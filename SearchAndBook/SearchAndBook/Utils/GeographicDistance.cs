6c93bc3c33e401a5ad81a8c83b7d810765730cc6
MasterplaYCoding <mateiursache2710@gmail.com>
2026-03-27 18:33:43 +0200

    Implemented initial state of the Distance classes

--- SearchAndBook/SearchAndBook/Utils/GeographicDistance.cs
+++ SearchAndBook/SearchAndBook/Utils/GeographicDistance.cs
@@ -1,22 +1,35 @@
-﻿using System;
+﻿using SearchAndBook.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+
 
 namespace SearchAndBook.Utils
 {
     public class GeographicDistance
     {
-        public int calculateDistance(String city1, String city2)
+        public double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
         {
-            /*
-             * Calculate the distance between two cities given by string, the return result is
-             * a integer representing the distance in KM
-             * 
-             * Until it's done you can call this function that just returns 0
-             */
-            return 0;
+            const double R = 6371;
+
+            var dLat = DegreesToRadians(lat2 - lat1);
+            var dLon = DegreesToRadians(lon2 - lon1);
+
+            var a =
+                Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(DegreesToRadians(lat1)) * Math.Cos(DegreesToRadians(lat2)) *
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return R * c;
         }
+
+        private double DegreesToRadians(double deg)
+            => deg * (Math.PI / 180);
     }
 }
+
