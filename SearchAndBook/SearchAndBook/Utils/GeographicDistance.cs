5b462280c342bc9d963c3eb97ff3375702b9a2b5
MasterplaYCoding <mateiursache2710@gmail.com>
2026-03-27 12:56:54 +0200

    Added the GeographicDistance class

---
+++ SearchAndBook/SearchAndBook/Utils/GeographicDistance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAndBook.Utils
+{
+    public class GeographicDistance
+    {
+        public int calculateDistance(String city1, String city2)
+        {
+            /*
+             * Calculate the distance between two cities given by string, the return result is
+             * a integer representing the distance in KM
+             * 
+             * Until it's done you can call this function that just returns 0
+             */
+            return 0;
+        }
+    }
+}
