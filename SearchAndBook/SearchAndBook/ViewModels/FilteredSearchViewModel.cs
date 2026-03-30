e9d12102dc0ffe1874ab07e7221c773b247e5d96
antonia1210 <antonia.szalok@stud.ubbcluj.ro>
2026-03-30 03:42:47 -0700

    viewmodel for search

--- SearchAndBook/SearchAndBook/ViewModels/FilteredSearchViewModel.cs
+++ SearchAndBook/SearchAndBook/ViewModels/FilteredSearchViewModel.cs
@@ -4,12 +4,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SearchAndBook.Shared;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using SearchAndBook.CommandHandler;
 
 namespace SearchAndBook.ViewModels
 {
-    internal class FilteredSearchViewModel
+    internal class FilteredSearchViewModel : INotifyPropertyChanged
     {
-        public ISearchAndFilterService SearchService { get; set; }
+        private readonly ISearchAndFilterService _searchService;
+        public List<GameDTO> Games { get; set; } = new();
+        public ObservableCollection<GameDTO> GamesShown { get; set; } = new();
+        private int _currentPage = 1;
+        public int CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+            }
+        }
+        public FilterCriteria Filter { get; set; } = new();
+        private const int PageSize = 10;
+        public ICommand SearchCommand { get; }
+        public ICommand NextPageCommand { get;}
+        public ICommand PreviousPageCommand { get; }
+        public void Initialize(FilterCriteria initialFilter)
+        {
+            Filter = initialFilter;
+            Search(Filter);
+        }
+        public FilteredSearchViewModel(ISearchAndFilterService searchService)
+        {
+            _searchService = searchService;
+            SearchCommand = new RelayCommand(_ => Search(Filter));
+            NextPageCommand = new RelayCommand(_ => NextPage());
+            PreviousPageCommand = new RelayCommand(_ => PreviousPage());
+        }
 
+        public void SelectGame(int gameId)
+        {
+            //null
+        }
+        public void Search(FilterCriteria criteria)
+        {
+            Games = _searchService.search(criteria);
+            CurrentPage = 1;
+            RefreshPage();
+        }
+        public void NextPage()
+        {
+            if (CurrentPage * PageSize < Games.Count)
+            {
+                CurrentPage++;
+                RefreshPage();
+            }
+        }
+        public void PreviousPage()
+        {
+            if (CurrentPage > 1)
+            {
+                CurrentPage--;
+                RefreshPage();
+             
+            }
+        }
+        private void RefreshPage()
+        {
+            GamesShown.Clear();
+            var pageListings = Games.Skip((CurrentPage - 1) * PageSize).Take(PageSize);
+            foreach (var game in pageListings)
+            {
+                GamesShown.Add(game);
+            }
+            OnPropertyChanged(nameof(GamesShown));
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ SearchAndBook/SearchAndBook/Views/FilteredSearchView.xaml.cs
@@ -0,0 +1,47 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using SearchAndBook.Services;
+using SearchAndBook.ViewModels;
+using SearchAndBook.Shared;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SearchAndBook.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FilteredSearchView : Page
+    {
+        public FilteredSearchView()
+        {
+            InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            ISearchAndFilterService searchService = new SearchAndFilterService(new Repositories.GameRepository(), new Repositories.RentalRepository());
+            var viewModel = new FilteredSearchViewModel(searchService);
+            if (e.Parameter is FilterCriteria criteriaFromFeed)
+            {
+                viewModel.Initialize(criteriaFromFeed);
+            }
+            this.DataContext = viewModel;
+
+        }
+    }
+}
