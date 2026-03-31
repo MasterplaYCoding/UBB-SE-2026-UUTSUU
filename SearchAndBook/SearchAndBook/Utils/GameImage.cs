c8692e407d2095424b48ff858064f4aa263c3adc
Untea Jessica <142442146+jessiixix@users.noreply.github.com>
2026-03-31 22:07:09 +0300

    added GameImage.cs

--- SearchAndBook/SearchAndBook/Utils/GameImage.cs
+++ SearchAndBook/SearchAndBook/Utils/GameImage.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Windows.Storage.Streams;
