65d44ceb7960aad72bac72ef2e822d3608b9e1fb
Untea Jessica <142442146+jessiixix@users.noreply.github.com>
2026-03-31 22:05:09 +0300

    Revert "deleted gameImage.cs"
    This reverts commit bcc725f9f1b3183e441b95ea57d73340851e4b90.

---
+++ SearchAndBook/SearchAndBook/Utils/GameImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
+
+namespace SearchAndBook.Utils
+{
+    class GameImage
+    {
+        public static async Task<BitmapImage?> ToBitmapImage(byte[]? imageBytes)
+        {
+            if (imageBytes == null || imageBytes.Length == 0)
+                return null;
+
+            using var stream = new InMemoryRandomAccessStream();
+            await stream.WriteAsync(imageBytes.AsBuffer());
+            stream.Seek(0);
+
+            var bitmap = new BitmapImage();
+            await bitmap.SetSourceAsync(stream);
+
+            return bitmap;
+        }
+    }
+}
