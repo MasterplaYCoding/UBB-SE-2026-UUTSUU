ff678c2cf0a9d343a524ae3685371dc16b971ea2
Untea Jessica <142442146+jessiixix@users.noreply.github.com>
2026-04-16 23:50:55 +0300

    Setup Test Project

--- RuntimeTerrors/Code/OurApp.Core/Database/DbConnectionHelper.cs
+++ RuntimeTerrors/Code/OurApp.Core/Database/DbConnectionHelper.cs
@@ -1,18 +1,18 @@
-using Microsoft.Data.SqlClient;
 using System;
+using Microsoft.Data.SqlClient;
 
 namespace OurApp.Core.Database
 {
     public static class DbConnectionHelper
     {
         // Derived from your provided Server instance name and the MyDb database
-        //Cip DB
-        //private const string ConnectionString = @"Server=(localdb)\ProjectModels;Database=MyDb;Trusted_Connection=True;TrustServerCertificate=True;";
+        // Cip DB
+        ////private const string ConnectionString = @"Server=(localdb)\ProjectModels;Database=MyDb;Trusted_Connection=True;TrustServerCertificate=True;";
         private const string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=iss_project;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
-        //private const string ConnectionString = "Data Source=DESKTOP-D8Q1P5P\\SQLEXPRESS;Initial Catalog=iss_project;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
-        //private const string ConnectionString =@"Server=(localdb)\ProjectModels;Database=MyDb;Trusted_Connection=True;TrustServerCertificate=True;";
-        //private const string ConnectionString = @"Data Source=Aron\\SQLEXPRESS;Initial Catalog=iss_project;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
-        //private const string ConnectionString = "Data Source=.;Initial Catalog=iss_project;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        ////private const string ConnectionString = "Data Source=DESKTOP-D8Q1P5P\\SQLEXPRESS;Initial Catalog=iss_project;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        ////private const string ConnectionString =@"Server=(localdb)\ProjectModels;Database=MyDb;Trusted_Connection=True;TrustServerCertificate=True;";
+        ////private const string ConnectionString = @"Data Source=Aron\\SQLEXPRESS;Initial Catalog=iss_project;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        ////private const string ConnectionString = "Data Source=.;Initial Catalog=iss_project;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
         public static SqlConnection GetConnection()
         {
