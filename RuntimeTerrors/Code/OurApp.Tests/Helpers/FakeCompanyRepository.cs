47f0274589b93d38e168cfc79ae8975a04d34e3c
victorutiu <victor.utiu@stud.ubbcluj.ro>
2026-04-22 10:45:19 +0300

    company service + validator tests

---
+++ RuntimeTerrors/Code/OurApp.Tests/Helpers/FakeCompanyRepository.cs
@@ -0,0 +1,63 @@
+﻿using OurApp.Core.Models;
+using OurApp.Core.Repositories;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace OurApp.Tests.Helpers
+{
+    public class FakeCompanyRepository : ICompanyRepo
+    {
+        public Company LastAdded;
+        public Company LastUpdated;
+        public int LastRemovedId;
+        public bool PrintAllCalled;
+        public Company StoredCompany;
+
+        public void Add(Company company)
+        {
+            LastAdded = company;
+        }
+
+        public Company GetById(int id)
+        {
+            return StoredCompany;
+        }
+
+        public void Update(Company company)
+        {
+            LastUpdated = company;
+        }
+
+        public void Remove(int id)
+        {
+            LastRemovedId = id;
+        }
+
+        public void PrintAll()
+        {
+            PrintAllCalled = true;
+        }
+
+        public Company GetCompanyByName(string name)
+        {
+            return StoredCompany;
+        }
+
+        public ObservableCollection<Company> GetAll()
+        {
+            return new ObservableCollection<Company>();
+        }
+
+        public Game GetGame()
+        {
+            return null;
+        }
+
+        public void SaveGame(Game game)
+        {
+
+        }
+
+
+    }
+}
